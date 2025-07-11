1a2dbb1b8bf0815fb7879d2ca867996461c02f16
kayllazahraa <168892439+kayllazahraa@users.noreply.github.com>
2025-07-11 22:32:20 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="text-center py-10 mt-16">
             <p className="text-slate-500 text-sm">
-                Created by Falana Rofako Hakam (222112038@stis.ac.id)
+                Created by Kaylla Zahrani (222313161@stis.ac.id)
             </p>
         </footer>
     );
