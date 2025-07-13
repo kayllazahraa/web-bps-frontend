6164913f33685fa624a6390b0c7f2058fa50b82e
kayllazahraa <168892439+kayllazahraa@users.noreply.github.com>
2025-07-13 20:19:47 +0700

    gradient login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -33,8 +33,7 @@ export default function LoginPage() {
   };
 
   return (
-    // Latar belakang abu-abu kebiruan
-    <div className="min-h-screen flex items-center justify-center bg-slate-50 px-4">
+    <div className="min-h-screen flex items-center justify-center px-4 bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900">
       {/* Kartu Login putih dengan bayangan */}
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
         <div className="flex flex-col items-center mb-6">
